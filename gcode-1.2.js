2ef2b0561e46c29a9cd035df93e84e39c48f9b4e
Daren Lee <90361336+thwerks@users.noreply.github.com>
2023-02-23 16:11:39 +0800

    Update gcode-1.2.js

--- gcode-1.2.js
+++ gcode-1.2.js
@@ -1,8 +1,8 @@
-let bedX = 250;         //set bed X dimension
+let bedX = 230;         //set bed X dimension
 let bedY = 230;         //set bed Y dimension
 let gantryX = 32;       //set X gantry height (distance from the bed to the underside of X Axis rail at its lowest printing position)
 let clearanceX = 31;    //set clearance from nozzle to fan shroud in the X axis
-let clearanceY = 68;    //set clearance from nozzle to fan shroud in the Y axis
+let clearanceY = 28;    //set clearance from nozzle to fan shroud in the Y axis
 let clearanceZ = 7;     //set clearance from the bed to the next obstruction on the carriage, usually the heatbreak or fan duct/shroud  
 let clearanceS = 2;     //set clearance between skirts (if part maxZ is below clearanceZ)
 let partStartX = 1;     //set begenning point for part alignment X (probably just leave to 5mm)
